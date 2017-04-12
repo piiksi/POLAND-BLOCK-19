9712541557755b865e140c397c331b3e6dab46f4
Paulina <95p.kowalczyk@gmail.com>
2017-04-12 21:00:57 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.see-comments').on('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $(this).closest('.hotel').find('.comments').slideToggle();
+  });
+});
+
+$('.hotel').on('keyup', '.quantity', function(event) {
+      var quantity = +$(this).val();
+      var price = +$(this).closest('.hotel').data('price');
+      $(this).closest('.hotel').find('.total').text( quantity * price );                
+});
